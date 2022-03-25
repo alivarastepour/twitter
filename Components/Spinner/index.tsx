ce21cd12dceaa80cce6be99f22afe94a130ced3b
ali varaste pour <alivaraste@outlook.com>
2022-03-25 01:45:01 -0700

    Spinner comoponent is added

---
+++ Components/Spinner/index.tsx
@@ -0,0 +1,10 @@
+import { Wrapper } from "./spinner.styles";
+
+const Spinner = () => {
+  return (
+    <>
+      <Wrapper />
+    </>
+  );
+};
+export default Spinner;
---
+++ Components/Spinner/spinner.styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 15px;
+  height: 15px;
+  border: 3px solid;
+  border-radius: 50%;
+  border-color: var(--twColor) var(--twColorLight) var(--twColorLight);
+  animation: spin 0.8s infinite linear;
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
