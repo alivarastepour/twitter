8c807c1e85aba8cf53024be83b70e189f7f52d15
ali varaste pour <alivaraste@outlook.com>
2022-03-25 01:44:29 -0700

    global styled component styles are added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import { GlobalStyle } from "../styles/global";
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default MyApp;
---
+++ styles/global.ts
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --twColor: rgb(29, 155, 240);
+    --twColorLight: rgb(138, 207, 253);
+  }
+`;
