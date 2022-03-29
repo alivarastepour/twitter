f69021faca4b962f5eca395821de5985070d1f98
ali varaste pour <alivaraste@outlook.com>
2022-03-29 02:27:35 -0700

    CustomDialoge component is added

---
+++ Components/Dialog/index.tsx
@@ -0,0 +1,49 @@
+import NativeDialogActions from "@mui/material/DialogActions";
+import NativeDialogContent from "@mui/material/DialogContent";
+import NativeDialogTitle from "@mui/material/DialogTitle";
+import NativeDialog from "@mui/material/Dialog";
+
+import styles from "../../styles/dialog.module.scss";
+
+const Dialog = ({ children }) => (
+  <>
+    <NativeDialog
+      open={true}
+      PaperProps={{
+        sx: {
+          width: "700px",
+          borderRadius: "15px",
+          "@media only screen and (max-width: 720px)": {
+            width: "100vw",
+            height: "100vh",
+          },
+        },
+      }}
+    >
+      {children}
+    </NativeDialog>
+  </>
+);
+const DialogTitle = ({ children }) => (
+  <NativeDialogTitle>
+    <div className={styles.titleContainer}>{children}</div>
+  </NativeDialogTitle>
+);
+
+const DialogContent = ({ children }) => (
+  <NativeDialogContent>
+    <div className={styles.formContainer}>{children}</div>
+  </NativeDialogContent>
+);
+
+const DialogActions = ({ children }) => (
+  <NativeDialogActions>
+    <div className={styles.actionContainer}>{children}</div>
+  </NativeDialogActions>
+);
+
+Dialog.Title = DialogTitle;
+Dialog.Content = DialogContent;
+Dialog.Actions = DialogActions;
+
+export default Dialog;
