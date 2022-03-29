7d0afc3a8c9daa582b60b72661a775212b26a0fb
ali varaste pour <alivaraste@outlook.com>
2022-03-29 03:32:03 -0700

    ThirdParty Button Wrapper is added

---
+++ Components/Buttons/ThirdPartyAuth.tsx
@@ -0,0 +1,18 @@
+import AppleLogo from "../Logo/Apple";
+import GoogleLogo from "../Logo/Google";
+import styles from "../../styles/ThirdPartyAuth.module.scss";
+
+const ThirdPartyAuth = ({ company, state }) => {
+  return (
+    <>
+      <div className={styles.logo}>
+        {company === "Apple" ? <AppleLogo /> : <GoogleLogo />}
+      </div>
+      <div className={` ${styles.text} text font font-eb`}>
+        Sign {state} with {company}
+      </div>
+    </>
+  );
+};
+
+export default ThirdPartyAuth;
