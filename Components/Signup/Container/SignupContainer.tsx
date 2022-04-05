a967b0b8d3b7f30e39d6071920f98344069b923d
Ali <alivaraste@outlook.com>
2022-04-05 11:40:03 +0430

    more concern separation is considered

--- Components/Signup/Container/SignupContainer.tsx
+++ Components/Signup/Container/SignupContainer.tsx
@@ -1,45 +1,56 @@
-import { useReducer } from "react";
+import {useReducer} from "react";
+
 import Signup from "../Presenter/Signup";
-import { reducer } from "../Handlers/signup.reducer";
+import {reducer} from "../Handlers/signup.reducer";
 import * as actions from "../Handlers/constants";
-import { YEAR, MONTH, calculateDaysCount } from "../Handlers/calenderHandler";
-import { validInfo } from "../Handlers/signup.handlers";
-import { TsignupFields } from "../Handlers/TsignupFields";
+import {YEAR, MONTH, calculateDaysCount} from "../Handlers/calenderHandler";
+import {TsignupFields} from "../Handlers/TsignupFields";
+import {
+    emailOnChange,
+    handleSignup,
+    nameOnBlur,
+    nameOnChange,
+    validInfo
+} from "../Handlers/signup.handlers";
 
 const SignupContainer = () => {
-  const initialState: TsignupFields = {
-    birthYear: "",
-    birthMonth: "",
-    birthDay: "",
-    name: "",
-    email: "",
-    nameError: "",
-    emailError: "",
-    serverError: "",
-  };
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const birthYearDispatch = (year: number): void => {
-    dispatch({ type: actions.SET_BIRTH_YEAR, payload: year });
-  };
-  const birthDayDispatch = (day: number): void => {
-    dispatch({ type: actions.SET_BIRTH_DAY, payload: day });
-  };
-  const birthMonthDispatch = (month: number): void => {
-    dispatch({ type: actions.SET_BIRTH_MONTH, payload: month });
-  };
-  return (
-    <Signup
-      state={state}
-      dispatch={dispatch}
-      actions={actions}
-      YEAR={YEAR}
-      MONTH={MONTH}
-      calculateDaysCount={calculateDaysCount}
-      validInfo={validInfo}
-      birthDayDispatch={birthDayDispatch}
-      birthMonthDispatch={birthMonthDispatch}
-      birthYearDispatch={birthYearDispatch}
-    />
-  );
+    const initialState: TsignupFields = {
+        birthYear: "",
+        birthMonth: "",
+        birthDay: "",
+        name: "",
+        email: "",
+        nameError: "",
+        emailError: "",
+        serverError: "",
+    };
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const birthYearDispatch = (year: number): void => {
+        dispatch({type: actions.SET_BIRTH_YEAR, payload: year});
+    };
+    const birthDayDispatch = (day: number): void => {
+        dispatch({type: actions.SET_BIRTH_DAY, payload: day});
+    };
+    const birthMonthDispatch = (month: number): void => {
+        dispatch({type: actions.SET_BIRTH_MONTH, payload: month});
+    };
+    return (
+        <Signup
+            state={state}
+            dispatch={dispatch}
+            actions={actions}
+            YEAR={YEAR}
+            MONTH={MONTH}
+            calculateDaysCount={calculateDaysCount}
+            validInfo={validInfo}
+            birthDayDispatch={birthDayDispatch}
+            birthMonthDispatch={birthMonthDispatch}
+            birthYearDispatch={birthYearDispatch}
+            emailOnChange={emailOnChange}
+            handleSignup={handleSignup}
+            nameOnBlur={nameOnBlur}
+            nameOnChange={nameOnChange}
+        />
+    );
 };
 export default SignupContainer;
--- Components/Signup/Presenter/Signup.tsx
+++ Components/Signup/Presenter/Signup.tsx
@@ -1,137 +1,130 @@
-import Link from "next/link";
-
-import IconButton from "@mui/material/IconButton";
 import TextField from "@mui/material/TextField";
-import CloseIcon from "@mui/icons-material/Close";
 
-import TwitterLogo from "../../Logo/Twitter";
 import Select from "../../Select";
 import Dialog from "../../Dialog";
 
 import styles from "../../../styles/SignupPage/signup.module.scss";
 
-import {
-  emailOnChange,
-  handleSignup,
-  nameOnBlur,
-  nameOnChange,
-} from "../Handlers/signup.handlers";
 
 const Signup = ({
-  state,
-  dispatch,
-  actions,
-  YEAR,
-  MONTH,
-  calculateDaysCount,
-  validInfo,
-  birthYearDispatch,
-  birthDayDispatch,
-  birthMonthDispatch,
-}) => {
-  return (
-    <>
-      <Dialog>
-        <Dialog.Title>
-          <Dialog.Header />
-          <div className="font font-eb">Create your account</div>
-        </Dialog.Title>
-        <Dialog.Content>
-          <div>
-            <TextField
-              fullWidth
-              label="Name"
-              sx={{ marginBottom: "30px" }}
-              error={state.nameError !== ""}
-              helperText={
-                <span style={{ color: "red" }}>{state.nameError}</span>
-              }
-              value={state.name}
-              onChange={(event) =>
-                nameOnChange(dispatch, actions.SET_NAME, event)
-              }
-              onBlur={(event) =>
-                nameOnBlur(dispatch, actions.SET_NAME_ERROR, event.target.value)
-              }
-            />
-          </div>
-          <div>
-            <TextField
-              fullWidth
-              label="Email"
-              sx={{ marginBottom: "30px" }}
-              error={state.emailError !== ""}
-              helperText={
-                <span style={{ color: "red" }}>{state.emailError}</span>
-              }
-              value={state.email}
-              onChange={(event) => {
-                emailOnChange(
-                  dispatch,
-                  actions.SET_EMAIL,
-                  actions.SET_EMAIL_ERROR,
-                  event.target.value
-                );
-              }}
-            />
-          </div>
-          <div className={`font font-b ${styles.date}`}>Date of birth</div>
-          <div className={`font font-l ${styles.dateNotice}`}>
-            This will not be shown publicly. Confirm your own age, even if this
-            account is for a business, a pet, or something else.
-          </div>
-          <div className={styles.dateContainer}>
-            <div className={styles.select}>
-              <Select
-                items={MONTH}
-                label="Month"
-                value={state.birthMonth}
-                setValue={birthMonthDispatch}
-                width={250}
-              />
-            </div>
-            <div className={styles.select}>
-              <Select
-                items={calculateDaysCount(state.birthYear, state.birthMonth)}
-                label="Day"
-                value={state.birthDay}
-                setValue={birthDayDispatch}
-                width={120}
-              />
-            </div>
-            <div className={styles.select}>
-              <Select
-                items={YEAR}
-                label="Year"
-                value={state.birthYear}
-                setValue={birthYearDispatch}
-                width={150}
-              />
-            </div>
-          </div>
-          <div className={`${styles.error} font font-m`}>
-            {state.serverError}
-          </div>
-        </Dialog.Content>
-        <Dialog.Actions>
-          <button
-            className={`font font-b ${styles.signupButton}`}
-            disabled={!validInfo(state)}
-            onClick={() =>
-              handleSignup(
-                dispatch,
-                actions.SET_SERVER_ERROR,
-                state.name,
-                state.email
-              )
-            }
-          >
-            Signup
-          </button>
-        </Dialog.Actions>
-      </Dialog>
-    </>
-  );
+                    state,
+                    dispatch,
+                    actions,
+                    YEAR,
+                    MONTH,
+                    calculateDaysCount,
+                    validInfo,
+                    birthYearDispatch,
+                    birthDayDispatch,
+                    birthMonthDispatch,
+                    emailOnChange,
+                    handleSignup,
+                    nameOnBlur,
+                    nameOnChange,
+                }) => {
+    return (
+        <>
+            <Dialog>
+                <Dialog.Title>
+                    <Dialog.Header/>
+                    <div className="font font-eb">Create your account</div>
+                </Dialog.Title>
+                <Dialog.Content>
+                    <div>
+                        <TextField
+                            fullWidth
+                            label="Name"
+                            sx={{marginBottom: "30px"}}
+                            error={state.nameError !== ""}
+                            helperText={
+                                <span style={{color: "red"}}>{state.nameError}</span>
+                            }
+                            value={state.name}
+                            onChange={(event) =>
+                                nameOnChange(dispatch, actions.SET_NAME, event)
+                            }
+                            onBlur={(event) =>
+                                nameOnBlur(dispatch, actions.SET_NAME_ERROR, event.target.value)
+                            }
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            fullWidth
+                            label="Email"
+                            sx={{marginBottom: "30px"}}
+                            error={state.emailError !== ""}
+                            helperText={
+                                <span style={{color: "red"}}>{state.emailError}</span>
+                            }
+                            value={state.email}
+                            onChange={(event) => {
+                                emailOnChange(
+                                    dispatch,
+                                    actions.SET_EMAIL,
+                                    actions.SET_EMAIL_ERROR,
+                                    event.target.value
+                                );
+                            }}
+                        />
+                    </div>
+                    <div className={`font font-b ${styles.date}`}>Date of birth</div>
+                    <div className={`font font-l ${styles.dateNotice}`}>
+                        This will not be shown publicly. Confirm your own age, even if this
+                        account is for a business, a pet, or something else.
+                    </div>
+                    <div className={styles.dateContainer}>
+                        <div className={styles.select}>
+                            <Select
+                                items={MONTH}
+                                label="Month"
+                                value={state.birthMonth}
+                                setValue={birthMonthDispatch}
+                                width={250}
+                            />
+                        </div>
+                        <div className={styles.select}>
+                            <Select
+                                items={calculateDaysCount(state.birthYear, state.birthMonth)}
+                                label="Day"
+                                value={state.birthDay}
+                                setValue={birthDayDispatch}
+                                width={120}
+                            />
+                        </div>
+                        <div className={styles.select}>
+                            <Select
+                                items={YEAR}
+                                label="Year"
+                                value={state.birthYear}
+                                setValue={birthYearDispatch}
+                                width={150}
+                            />
+                        </div>
+                    </div>
+                    <div className={`${styles.error} font font-m`}>
+                        {state.serverError}
+                    </div>
+                </Dialog.Content>
+                <Dialog.Actions>
+                    <button
+                        className={`font font-b ${styles.signupButton}`}
+                        disabled={!validInfo(state)}
+                        onClick={() =>
+                            handleSignup(
+                                dispatch,
+                                actions.SET_SERVER_ERROR,
+                                state.name,
+                                state.email
+                            )
+                        }
+                    >
+                        Signup
+                    </button>
+                </Dialog.Actions>
+            </Dialog>
+        </>
+    );
 };
 
 export default Signup;
