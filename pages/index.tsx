317ea3f53ab211e8d7b950bf5d00f16d2e084797
Ali <alivaraste@outlook.com>
2022-04-05 06:54:25 +0430

    style for error container is added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import Mainpage from "../Components/Mainpage";
 import PageWrapper from "../Components/PageWrapper/PageWrapper";
 
 const HomePage = () => (
-  <>
-    <PageWrapper
-      title="Twitter. it's what's happening / Twitter"
-      Component={Mainpage}
-    />
-  </>
+    <>
+        <PageWrapper
+            title="Twitter. it's what's happening / Twitter"
+            Component={Mainpage}
+        />
+    </>
 );
 export default HomePage;
