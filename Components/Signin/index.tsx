09b17b9e717199f2d32f516ca891cbb2b50220c3
Ali <alivaraste@outlook.com>
2022-04-05 06:53:58 +0430

    signin logic is handled

--- Components/Signin/index.tsx
+++ Components/Signin/index.tsx
@@ -1,82 +1,107 @@
 import Dialog from "../Dialog";
 import styles from "../../styles/SigninPage/signin.module.scss";
 import ThirdPartyAuth from "../Buttons/ThirdPartyAuth";
-import { Button } from "../Buttons/MainpageButtonWrapper";
+import {Button} from "../Buttons/MainpageButtonWrapper";
 import buttonStyles from "../../styles/Button/button.module.scss";
 import Divider from "../Divider";
-import { TextField } from "@mui/material";
+import {TextField} from "@mui/material";
 import Link from "next/link";
+import {useState} from "react";
+import {handleSignin} from "./signin.handlers";
+import {TsigninFields} from "./TsigninFields";
 
 const Signin = () => {
-  return (
-    <>
-      <Dialog>
-        <Dialog.Title>
-          <Dialog.Header />
-          <div className={`font font-eb ${styles.header}`}>
-            Sign in to Twitter
-          </div>
-        </Dialog.Title>
-        <Dialog.Content>
-          <div className={styles.contentContainer}>
-            <div className={styles.contentItem}>
-              <Button className={buttonStyles.whiteButton}>
-                <ThirdPartyAuth state="in" company="Google" />
-              </Button>
-            </div>
-            <div className={styles.contentItem}>
-              <Button className={buttonStyles.whiteButton}>
-                <ThirdPartyAuth state="in" company="Apple" />
-              </Button>
-            </div>
-            <div className={styles.contentItem}>
-              <Divider orientation="horizontal" text="or" width={300} />
-            </div>
-            <div className={styles.contentItem}>
-              <TextField
-                sx={{ width: 300 }}
-                label={<div className="font">username</div>}
-              />
-            </div>
-            <div className={styles.contentItem}>
-              <TextField
-                sx={{ width: 300 }}
-                label={<div className="font">password</div>}
-              />
-            </div>
-            <div className={styles.contentItem}>
-              <Button
-                className={`
+
+    const signinInitialState: TsigninFields = {
+        email: "",
+        password: "",
+        clientError: "",
+        serverError: "",
+    }
+    const [siginState, setSigninState] = useState(signinInitialState);
+
+    return (
+        <>
+            <Dialog>
+                <Dialog.Title>
+                    <Dialog.Header/>
+                    <div className={`font font-eb ${styles.header}`}>
+                        Sign in to Twitter
+                    </div>
+                </Dialog.Title>
+                <Dialog.Content>
+                    <div className={styles.contentContainer}>
+                        <div className={styles.contentItem}>
+                            <Button className={buttonStyles.whiteButton}>
+                                <ThirdPartyAuth state="in" company="Google"/>
+                            </Button>
+                        </div>
+                        <div className={styles.contentItem}>
+                            <Button className={buttonStyles.whiteButton}>
+                                <ThirdPartyAuth state="in" company="Apple"/>
+                            </Button>
+                        </div>
+                        <div className={styles.contentItem}>
+                            <Divider orientation="horizontal" text="or" width={300}/>
+                        </div>
+                        <div className={styles.contentItem}>
+                            <TextField
+                                sx={{width: 300}}
+                                label={<div className="font">email</div>}
+                                value={siginState.email}
+                                onChange={e => setSigninState(prevState => {
+                                    return {...prevState, email: e.target.value}
+                                })}
+                            />
+                        </div>
+                        <div className={styles.contentItem}>
+                            <TextField
+                                sx={{width: 300}}
+                                label={<div className="font">password</div>}
+                                value={siginState.password}
+                                onChange={e => setSigninState(prevState => {
+                                    return {...prevState, password: e.target.value}
+                                })}
+                            />
+                        </div>
+                        {(siginState.clientError || siginState.serverError) && <div className={styles.contentItem}>
+                            <div className={styles.error + " font font-m"}>
+                                {siginState.clientError ? siginState.clientError : siginState.serverError ? siginState.serverError : ""}
+                            </div>
+                        </div>}
+                        <div className={styles.contentItem} onClick={() => handleSignin(siginState, setSigninState)}>
+                            <Button
+                                className={`
               ${buttonStyles.blackButton} font font-eb`}
-              >
-                Signin
-              </Button>
-            </div>
-            <div className={styles.contentItem}>
-              <Button
-                className={`
+                            >
+                                Signin
+                            </Button>
+                        </div>
+                        <div className={styles.contentItem}>
+                            <Button
+                                className={`
               ${buttonStyles.whiteButton} font font-b`}
-              >
-                Forgot password?
-              </Button>
-            </div>
-            <div
-              className={`${
-                styles.contentItem + " " + styles.help
-              } font font-m`}
-            >
-              Don&apos;t have an account?
-              <div style={{ display: "inline-block" }}>
-                <Link href="/signup" passHref>
-                  <div className={styles.signup}>Sign up</div>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </Dialog.Content>
-      </Dialog>
-    </>
-  );
+                            >
+                                Forgot password?
+                            </Button>
+                        </div>
+                        <div
+                            className={`${
+                                styles.contentItem + " " + styles.help
+                            } font font-m`}
+                        >
+                            Don&apos;t have an account?
+                            <div style={{display: "inline-block"}}>
+                                <Link href="/signup" passHref>
+                                    <div className={styles.signup}>Sign up</div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </Dialog.Content>
+            </Dialog>
+        </>
+    );
 };
 
 export default Signin;
---
+++ Components/Signin/signin.handlers.ts
@@ -0,0 +1,42 @@
+import {TsigninFields} from "./TsigninFields";
+import axios from "axios";
+import {HOST} from "../../public/host";
+
+export const handleSignin = (state: TsigninFields, setState: Function): void => {
+    if (state.email.trim() === "") {
+        setState(prev => {
+            return {...prev, clientError: "email can not be empty"}
+        })
+        return;
+    }
+    if (state.password.trim() === "") {
+        setState(prev => {
+            return {...prev, clientError: "password can not be empty"}
+        })
+        return;
+    }
+    const url: string = `${HOST}/users/login`;
+    const data = {
+        user: {
+            email: state.email,
+            password: state.password
+        }
+    };
+    axios.post(url, data).then(res => {
+        localStorage.setItem('__ut', res.data.user.token);
+        setState(prev => {
+            return {...prev, serverError: "", clientError: ""}
+        })
+    }).catch(err => {
+        let errorMessage: string = "";
+        try {
+            errorMessage = err.response.data.errors.body;
+            errorMessage = errorMessage === 'access forbidden' ? 'wrong username or password' : errorMessage;
+        } catch (error) {
+            errorMessage = "failed to login.";
+        }
+        setState(prev => {
+            return {...prev, serverError: errorMessage}
+        })
+    });
+}
