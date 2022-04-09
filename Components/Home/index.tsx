47c56e920a72b4f2fdac7f619c73a0451c16fa0f
Ali <alivaraste@outlook.com>
2022-04-09 22:27:18 +0430

    home component is added

---
+++ Components/Home/index.tsx
@@ -0,0 +1,4 @@
+const Home = () => {
+    return <>salam man home hastam</>
+}
+export default Home;
---
+++ pages/home.tsx
@@ -0,0 +1,10 @@
+import PageWrapper from "../Components/PageWrapper/PageWrapper";
+import Home from "../Components/Home";
+
+const HomePage = () => (
+    <PageWrapper
+    Component={Home}
+    title='() Home / Twitter'
+    />
+)
+export default HomePage;
