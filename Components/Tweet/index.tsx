db6d7f243e359bcda14aebe441652dd4dc3503eb
Ali <alivaraste@outlook.com>
2022-05-07 11:20:31 +0430

    tweet input field is added

--- Components/Tweet/index.tsx
+++ Components/Tweet/index.tsx
@@ -1,21 +1,45 @@
 import {Avatar} from "@mui/material";
+import {Wrapper} from './tweet.styles'
+import {useState} from "react";
 
 const Tweet = ({who}) => {
-    let shortForm:string ;
-    let separated:string[] = who.split(' ') ;
+    console.log("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss".length)
+    const [tweetText, setTweetText] = useState('');
+    let shortForm: string;
+    let separated: string[] = who.split(' ');
     if (separated.length > 1)
         shortForm = who.split(' ')[0][0] + who.split(' ')[1][0]
     else
         shortForm = who[0]
+
+    const calculateRows = () => {
+        const value = Math.ceil(tweetText.length / 51);
+        if (value !== 0)
+            return value;
+        return 1
+    }
     return <>
-        <div className='tweetInput'>
-            <div>
-                <Avatar sx={{bgcolor: 'violet'}}>{shortForm.toUpperCase()}</Avatar>
+        <Wrapper>
+            <div className='tweetInput'>
+                <div>
+                    <Avatar sx={{bgcolor: 'violet', width: 56, height: 56}}>
+                        <div className='avatarText'>
+                            {shortForm.toUpperCase()}
+                        </div>
+                    </Avatar>
+                </div>
+                <div>
+                    <textarea value={tweetText} onChange={e => {
+                        if (e.target.value.length == 280)
+                            e.preventDefault();
+                        else
+                            setTweetText(e.target.value)
+                    }
+                    } className='textArea font font-m' cols={45} rows={calculateRows()}/>
+                </div>
             </div>
-            <div>
-                <input type="text"/>
-            </div>
-        </div>
+        </Wrapper>
+
     </>
 }
 export default Tweet;
--- Components/Tweet/tweet.styles.ts
+++ Components/Tweet/tweet.styles.ts
@@ -1,8 +1,25 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+export const Wrapper = styled.div`
+  
   .tweetInput{
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    padding: 15px;
   }
+  
+  .textArea{
+    resize: none;
+    border: none;
+    outline: none;
+    font-size: 1.4rem;
+    //margin-left: 10px;
+    //margin-top: 10px;
+    margin: 10px 10px auto;
+  }
+  
+  .avatarText{
+    font-size: 1.8rem;
+  }
+  
 `;
