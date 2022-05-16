cf54538eb2eae1bd17ccfef248c666ef90b45268
Ali <alivaraste@outlook.com>
2022-05-16 20:12:46 +0430

    more styles for tweetDisplay component is added

--- Components/tweetDisplay/index.tsx
+++ Components/tweetDisplay/index.tsx
@@ -5,7 +5,11 @@ import ModeCommentOutlinedIcon from '@mui/icons-material/ModeCommentOutlined';
 import RepeatOutlinedIcon from '@mui/icons-material/RepeatOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
+import {useState} from "react";
+import {Tooltip} from "@mui/material";
+
 const TweetDisplay = () => {
+    const [hoverEffect, setHoverEffect] = useState('');
     return <>
         <Wrapper>
             <div className="tweetHeader">
@@ -16,41 +20,50 @@ const TweetDisplay = () => {
                     <span className='tweetOwnerName font font-b'>ali varaste pour</span>
                     <span className='tweetOwnerUsername font font-m'>@AliVarastePour</span>
                     <span> . </span>
-                    <span className='tweetTime font font-m'>20h</span>
+                    <Tooltip title={<div style={{fontSize: '0.8rem'}} className='font font-m'>saturday august 12th,
+                        2022</div>}>
+                        <span className='tweetTime font font-m'>20h</span>
+                    </Tooltip>
                 </div>
             </div>
             <div className='tweetContent font font-m'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, alias consequatur corporis distinctio dolores doloribus hic labore minus numquam quaerat, quis repudiandae tenetur! Blanditiis culpa doloremque exercitationem minus vel, voluptatibus!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, alias consequatur corporis distinctio
+                dolores doloribus hic labore minus numquam quaerat, quis repudiandae tenetur! Blanditiis culpa
+                doloremque exercitationem minus vel, voluptatibus!
             </div>
             <div className="tweetFooter">
-                <div className="tweetAction">
+                <div className="tweetAction comment" onMouseEnter={() => setHoverEffect('comment')}
+                     onMouseLeave={() => setHoverEffect('')}>
                     <div className="actionIcon">
                         <IconButton>
-                            <ModeCommentOutlinedIcon/>
+                            <ModeCommentOutlinedIcon sx={hoverEffect === 'comment' && {color: 'rgb(29, 155, 240)'}}/>
                         </IconButton>
                     </div>
-                    <div className="actionText">100</div>
+                    <div className="actionText font font-m">100</div>
                 </div>
-                <div className="tweetAction">
+                <div className="tweetAction retweet" onMouseEnter={() => setHoverEffect('retweet')}
+                     onMouseLeave={() => setHoverEffect('')}>
                     <div className="actionIcon">
                         <IconButton>
-                            <RepeatOutlinedIcon/>
+                            <RepeatOutlinedIcon sx={hoverEffect === 'retweet' && {color: 'rgb(0, 186, 124)'}}/>
                         </IconButton>
                     </div>
-                    <div className="actionText">213</div>
+                    <div className="actionText font font-m">213</div>
                 </div>
-                <div className="tweetAction">
+                <div className="tweetAction like" onMouseEnter={() => setHoverEffect('like')}
+                     onMouseLeave={() => setHoverEffect('')}>
                     <div className="actionIcon">
                         <IconButton>
-                            <FavoriteBorderOutlinedIcon/>
+                            <FavoriteBorderOutlinedIcon sx={hoverEffect === 'like' && {color: 'rgb(249, 24, 128)'}}/>
                         </IconButton>
                     </div>
-                    <div className="actionText">31</div>
+                    <div className="actionText font font-m">31</div>
                 </div>
-                <div className="tweetAction">
+                <div className="tweetAction" onMouseEnter={() => setHoverEffect('share')}
+                     onMouseLeave={() => setHoverEffect('')}>
                     <div className="actionIcon">
                         <IconButton>
-                            <FileUploadOutlinedIcon/>
+                            <FileUploadOutlinedIcon sx={hoverEffect === 'share' && {color: 'rgb(29, 155, 240)'}}/>
                         </IconButton>
                     </div>
                 </div>
--- Components/tweetDisplay/tweetDisplay.styles.ts
+++ Components/tweetDisplay/tweetDisplay.styles.ts
@@ -5,45 +5,94 @@ export const Wrapper = styled.div`
   border: 1px solid black;
   height: fit-content;
   margin-top: 20px;
-  
-  .tweetHeader{
+  cursor: pointer;
+
+  &:hover {
+    transition: 0.2s;
+    background-color: rgba(0, 0, 0, 0.03);
+  }
+
+  &:not(:hover) {
+    transition: 0.2s;
+    background-color: white;
+  }
+
+  .tweetHeader {
     display: flex;
     flex-direction: row;
     align-items: stretch;
 
-    .ownerContent{
+    .ownerContent {
       margin-left: 10px;
+      .timeTooltip{
+        color: red;
+      }
     }
-    
-    .tweetOwnerName{
+
+    .tweetOwnerName {
       font-size: 1.1rem;
       margin-right: 10px;
+
+      &:hover {
+        text-decoration: underline;
+      }
+
     }
-    
-    .tweetOwnerUsername, .tweetTime{
+
+    .tweetOwnerUsername, .tweetTime {
       color: #536471;
     }
-    
+
+    .tweetTime {
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+
   }
-  
-  .tweetContent{
+
+  .tweetContent {
     margin-left: 60px;
     margin-top: -20px;
     color: #0F1419;
     line-height: 1.4rem;
   }
-  
-  .tweetFooter{
+
+  .tweetFooter {
     display: flex;
     flex-direction: row;
-    justify-content: center;
-    
-    .tweetAction{
+    justify-content: space-around;
+
+    .tweetAction {
       display: flex;
       flex-direction: row;
       align-items: center;
-      justify-content: space-evenly;
+      justify-content: space-around;
     }
-    
+
+    .comment {
+      color: rgb(83, 100, 113);
+
+      &:hover {
+        color: rgb(29, 155, 240);
+      }
+    }
+
+    .retweet {
+      color: rgb(83, 100, 113);
+
+      &:hover {
+        color: rgb(0, 186, 124);
+      }
+
+    }
+
+    .like {
+      color: rgb(83, 100, 113);
+
+      &:hover {
+        color: rgb(249, 24, 128);
+      }
+    }
   }
 `;
