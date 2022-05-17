ff16580cc4200b9453650f93997cc58813667d86
Ali <alivaraste@outlook.com>
2022-05-17 12:13:39 +0430

    fixed position for sideBar is added

--- Components/Sidebar/index.tsx
+++ Components/Sidebar/index.tsx
@@ -1,6 +1,10 @@
+import Link from 'next/link'
+
 import List from "@mui/material/List";
-import {ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
-import styles from "../../styles/Home/sidebar.module.scss";
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import TagRoundedIcon from "@mui/icons-material/TagRounded";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
@@ -9,11 +13,15 @@ import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlin
 import ListAltOutlinedIcon from "@mui/icons-material/ListAltOutlined";
 import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import {useState} from "react";
+
 import TwitterLogo from "../Logo/Twitter";
-import {sidebarContext} from "./sidebarContext";
-import Link from 'next/link'
+
+import {Wrapper} from './sideBar.styles'
+
+import styles from "../../styles/Home/sidebar.module.scss";
 import button from '../../styles/Button/button.module.scss';
+
+import {sidebarContext} from "./sidebarContext";
 
 const getIcon = (title) => {
     let Icon = null;
@@ -44,64 +52,64 @@ const getIcon = (title) => {
 }
 
 const Sidebar = ({selected, select}) => {
-
-
     return <>
-        <List>
-            <ListItem>
-                <div style={{marginLeft: 10}}>
-                    <ListItemButton sx={{
-                        '&:hover': {
-                            background: 'rgba(29, 155, 240, 0.1)',
-                        }, borderRadius: '50%', width: 65, height: 65, display: 'flex', paddingLeft: 1
-                    }}>
-                        <ListItemIcon>
-                            <TwitterLogo size='S'/>
-                        </ListItemIcon>
-                    </ListItemButton>
-                </div>
-            </ListItem>
-            {
-                sidebarContext.map(context => {
-                    return <ListItem key={context.id}>
-                        <Link href={context.link} passHref>
-                            <div>
-                                <ListItemButton sx={{borderRadius: 20, paddingRight: 5}} className={styles.listItem}
-                                                onClick={() => select(context.title)}>
-                                    <ListItemIcon>
-                                        {getIcon(context.title)}
-                                    </ListItemIcon>
-                                    <ListItemText>
-                                        <div
-                                            className={`font ${styles.listItemText} + ${selected == context.title ? 'font-eb' : ''}`}>
-                                            {context.title}
-                                        </div>
-                                    </ListItemText>
-                                </ListItemButton>
-                            </div>
-                        </Link>
-                    </ListItem>
-                })
-            }
-            <ListItem>
-                <div>
-                    <ListItemButton sx={{borderRadius: 20, paddingRight: 5}} className={styles.listItem}
-                                    onClick={() => select('more')}>
-                        <ListItemIcon>
-                            <MoreHorizIcon sx={{width: 35, height: 35, color: 'black'}}/>
-                        </ListItemIcon>
-                        <ListItemText>
-                            <div className={`font ${styles.listItemText} + ${selected == 'more' ? 'font-eb' : ''}`}>
-                                More
-                            </div>
-                        </ListItemText>
-                    </ListItemButton>
-                </div>
-            </ListItem>
-            <ListItem>
-                <button className={button.blueButton + ' ' + styles.tweet + ' font font-b'}>Tweet</button>
-            </ListItem>
-        </List>
+        <Wrapper>
+            <List>
+                <ListItem>
+                    <div style={{marginLeft: 10}}>
+                        <ListItemButton sx={{
+                            '&:hover': {
+                                background: 'rgba(29, 155, 240, 0.1)',
+                            }, borderRadius: '50%', width: 65, height: 65, display: 'flex', paddingLeft: 1
+                        }}>
+                            <ListItemIcon>
+                                <TwitterLogo size='S'/>
+                            </ListItemIcon>
+                        </ListItemButton>
+                    </div>
+                </ListItem>
+                {
+                    sidebarContext.map(context => {
+                        return <ListItem key={context.id}>
+                            <Link href={context.link} passHref>
+                                <div>
+                                    <ListItemButton sx={{borderRadius: 20, paddingRight: 5}} className={styles.listItem}
+                                                    onClick={() => select(context.title)}>
+                                        <ListItemIcon>
+                                            {getIcon(context.title)}
+                                        </ListItemIcon>
+                                        <ListItemText>
+                                            <div
+                                                className={`font ${styles.listItemText} + ${selected == context.title ? 'font-eb' : ''}`}>
+                                                {context.title}
+                                            </div>
+                                        </ListItemText>
+                                    </ListItemButton>
+                                </div>
+                            </Link>
+                        </ListItem>
+                    })
+                }
+                <ListItem>
+                    <div>
+                        <ListItemButton sx={{borderRadius: 20, paddingRight: 5}} className={styles.listItem}
+                                        onClick={() => select('more')}>
+                            <ListItemIcon>
+                                <MoreHorizIcon sx={{width: 35, height: 35, color: 'black'}}/>
+                            </ListItemIcon>
+                            <ListItemText>
+                                <div className={`font ${styles.listItemText} + ${selected == 'more' ? 'font-eb' : ''}`}>
+                                    More
+                                </div>
+                            </ListItemText>
+                        </ListItemButton>
+                    </div>
+                </ListItem>
+                <ListItem>
+                    <button className={button.blueButton + ' ' + styles.tweet + ' font font-b'}>Tweet</button>
+                </ListItem>
+            </List>
+        </Wrapper>
     </>
 }
 
