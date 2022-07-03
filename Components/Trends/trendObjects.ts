29ed9d76763dcf1c9965ae6f410cf329b446fe74
Ali <alivaraste@outlook.com>
2022-07-03 11:15:25 +0430

    trend static data is generated

---
+++ Components/Trends/trendObjects.ts
@@ -0,0 +1,12 @@
+export const trendObjects = [
+    {title: 'Trending in Germany', content: '#Gaslieferungen', count: '1058'},
+    {title: 'Trending in Germany', content: 'Trinkgeld', count: ''},
+    {title: 'Trending in Germany', content: '#colognepride', count: ''},
+    {title: 'Arts & culture · Trending', content: 'Franz Kafka', count: '2409'},
+    {title: 'Trending in Germany', content: '#CancelCulture', count: '1860'},
+    {title: 'Entertainment · Trending', content: '#BLEACH_anime', count: '69.1k'},
+    {title: 'Trending in Technology', content: 'Jeff', count: '49.8k'},
+    {title: 'Sports · Trending', content: '#Ronaldo', count: '26.6k'},
+    {title: 'Trending in Germany', content: 'Maßnahmen', count: '23.1k'},
+    {title: 'Trending in Germany', content: '#Selbstbestimmungsgesetz', count: '3508'},
+]
