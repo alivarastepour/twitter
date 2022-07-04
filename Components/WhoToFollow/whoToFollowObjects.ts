0e522a9f3a2ac5cf009ae7b86e90bb09c7133070
Ali <alivaraste@outlook.com>
2022-07-04 10:59:11 +0430

    WhoToFollow fake data is added

---
+++ Components/WhoToFollow/whoToFollowObjects.ts
@@ -0,0 +1,5 @@
+export const whoToFollowObjects = [
+    {avatar:'/Nyn1HZWF_400x400.jpg', name: 'Elon Musk', username: '@elonmusk'},
+    {avatar:'/JdS8yLGI_400x400.jpg', name: 'Bill Gates', username: '@BillGates'},
+    {avatar:'/Wmyak8D6_400x400.jpg', name: 'NASA Webb Telescope', username: '@NASAWebb'},
+]
