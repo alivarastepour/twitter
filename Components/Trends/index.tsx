57f5b0df39d306e3f0fcf531213a36257e6ce792
Ali <alivaraste@outlook.com>
2022-07-05 17:57:45 +0430

    file cleanup(Trends section)

--- Components/Trends/index.tsx
+++ Components/Trends/index.tsx
@@ -1,20 +1,21 @@
-import {Wrapper} from './trends.styles'
 import TrendItem from "../TrendItem";
+
+import {Wrapper} from './trends.styles';
+
 import {trendObjects} from "./trendObjects";
 
-const Trends = () => {
-    return <>
-        <Wrapper>
-            <div className='font font-eb title'>Trends for you</div>
-            <div>
-                {
-                    trendObjects.map(trendObject => {
-                        const {title, content, count} = trendObject;
-                        return <TrendItem title={title} content={content} count={count} key={title + content}/>
-                    })
-                }
-            </div>
-        </Wrapper>
-    </>
-}
+const Trends = () => (
+    <Wrapper>
+        <div className='font font-eb title'>Trends for you</div>
+        <div>
+            {
+                trendObjects.map(trendObject => {
+                    const {title, content, count} = trendObject;
+                    return <TrendItem title={title} content={content} count={count} key={title + content}/>
+                })
+            }
+        </div>
+    </Wrapper>
+)
+
 export default Trends;
