f24b2a4c0afd741a4f85008487bd2654fe78938c
Ali <alivaraste@outlook.com>
2022-07-15 17:28:24 +0430

    redux store is being provided

--- Components/HomePageWrapper/index.tsx
+++ Components/HomePageWrapper/index.tsx
@@ -4,31 +4,33 @@ import SearchBar from "../SearchBar";
 import Trends from "../Trends";
 import WhoToFollow from "../WhoToFollow";
 import Footer from "../Home/Footer";
-import {useState} from "react";
 import PageWrapper from "../PageWrapper/PageWrapper";
+import {Provider} from "react-redux";
+import store from "../../store/store";
 
 const HomePageWrapper = ({Component, title, props}) => {
-    const [selected, select] = useState('home');
 
     return <>
         <Wrapper>
-            <div className='left'>
-                <Sidebar selected={selected} select={select}/>
-            </div>
-            <div className="main">
-                <PageWrapper
-                    title={title}
-                    props={props}
-                    Component={Component}
-                />
-            </div>
+            <Provider store={store}>
+                <div className='left'>
+                    <Sidebar/>
+                </div>
+                <div className="main">
+                    <PageWrapper
+                        title={title}
+                        props={props}
+                        Component={Component}
+                    />
+                </div>
 
-            <div className='right'>
-                <SearchBar/>
-                <Trends/>
-                <WhoToFollow/>
-                <Footer/>
-            </div>
+                <div className='right'>
+                    <SearchBar/>
+                    <Trends/>
+                    <WhoToFollow/>
+                    <Footer/>
+                </div>
+            </Provider>
         </Wrapper>
     </>
 }
