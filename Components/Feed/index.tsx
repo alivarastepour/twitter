b2313a2c0f63d9b1685083664520a5b9a9e9256b
Ali <alivaraste@outlook.com>
2022-07-20 18:33:43 +0430

    Feed and Profile components are rewriten using useFetch component

--- Components/Feed/index.tsx
+++ Components/Feed/index.tsx
@@ -1,48 +1,37 @@
 import Tweet from "../Tweet";
 import TweetDisplay from "../tweetDisplay";
 import Spinner from "../Spinner";
-import useSWR from "swr";
-import {HOST} from "../../public/host";
-import {useCallback} from "react";
-import axios from "axios";
+import { HOST } from "../../public/host";
 import useSidebar from "../../customHooks/useSidebar";
+import useFetch from "../../customHooks/useFetch";
 
 const Feed = () => {
-
-    useSidebar({payload: 'home'});
-
-    const fetchUserInfo = useCallback((url: string): Promise<any> => {
-        return axios.get(url, {
-            headers: {
-                authorization: `Token ${localStorage.getItem('__ut')}`
-            }
+  useSidebar({ payload: "home" });
+  const { data: userInfo } = useFetch(`${HOST}/user`);
+  const { data: tweets, isValidating, error } = useFetch(`${HOST}/articles`);
+  return (
+    <>
+      <Tweet isModal={false} who={userInfo?.data?.user?.username || "?"} />
+      {!isValidating && !error ? (
+        tweets.data.articles.map((tweet) => {
+          return (
+            <TweetDisplay
+              tweet={tweet?.body}
+              avatarURL={tweet?.author?.image}
+              name={tweet?.author?.username}
+              username={tweet?.author?.username}
+              time={tweet?.createdAt}
+              key={tweet?.createdAt}
+              bio={tweet?.author?.bio}
+            />
+          );
         })
-    }, [])
-
-    const fetchTweets = useCallback((url: string): Promise<any> => {
-        return axios.get(url, {
-            headers: {
-                authorization: `Token ${localStorage.getItem('__ut')}`
-            }
-        })
-    }, [])
-    const userInfo = useSWR(`${HOST}/user`, fetchUserInfo);
-    const {data, isValidating, error} = useSWR(`${HOST}/articles`, fetchTweets);
-    return <>
-        <Tweet isModal={false}
-               who={userInfo?.data?.data?.user?.username || '?'}/>
-
-        {
-            !isValidating && !error ? data.data.articles.map(tweet => {
-                return <TweetDisplay tweet={tweet?.body}
-                                     avatarURL={tweet?.author?.image}
-                                     name={tweet?.author?.username}
-                                     username={tweet?.author?.username}
-                                     time={tweet?.createdAt} key={tweet?.createdAt}
-                                     bio={tweet?.author?.bio}
-                />
-            }) : error ? <></> : <Spinner/>
-        }
+      ) : error ? (
+        <></>
+      ) : (
+        <Spinner />
+      )}
     </>
-}
+  );
+};
 export default Feed;
--- Components/Profile/index.tsx
+++ Components/Profile/index.tsx
@@ -1,13 +1,18 @@
-import axios from "axios";
-import {HOST} from "../../public/host";
-import useSWR from "swr";
-import useSidebar from "../../customHooks/useSidebar";
+import { Dispatch } from "@reduxjs/toolkit";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import useFetch from "../../customHooks/useFetch";
+import { HOST } from "../../public/host";
 
-const Profile = ({data}) => {
-    useSidebar({payload: 'profile'});
-    console.log(data);
-    return <>salam</>
-}
-
+const Profile = ({ username }) => {
+  const dispatch: Dispatch<any> = useDispatch();
+  useEffect(() => {
+    dispatch({ type: "sidebar/select", payload: "profile" });
+  }, []);
+  const url: string =
+    username === "profile" ? `${HOST}/user` : `${HOST}/profiles/${username}`;
+  const { data: userInfo } = useFetch(url);
+  return <>salam</>;
+};
 
 export default Profile;
