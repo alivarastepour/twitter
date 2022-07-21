2dbba538ee4e4e40849e89fcc12ed6203478b513
Ali <alivaraste@outlook.com>
2022-07-21 22:35:15 +0430

    prop type is added to ThirdPartyAuth

--- Components/Buttons/ThirdPartyAuth.tsx
+++ Components/Buttons/ThirdPartyAuth.tsx
@@ -1,18 +1,19 @@
 import AppleLogo from "../Logo/Apple";
 import GoogleLogo from "../Logo/Google";
+
+import { TthirdPartyAuth } from "./TthirdPartyAuth";
+
 import styles from "../../styles/Utils/ThirdPartyAuth.module.scss";
 
-const ThirdPartyAuth = ({ company, state }) => {
-  return (
-    <>
-      <div className={styles.logo}>
-        {company === "Apple" ? <AppleLogo /> : <GoogleLogo />}
-      </div>
-      <div className={` ${styles.text} text font font-eb`}>
-        Sign {state} with {company}
-      </div>
-    </>
-  );
-};
+const ThirdPartyAuth = ({ company, state }: TthirdPartyAuth) => (
+  <>
+    <div className={styles.logo}>
+      {company === "Apple" ? <AppleLogo /> : <GoogleLogo />}
+    </div>
+    <div className={` ${styles.text} text font font-eb`}>
+      Sign {state} with {company}
+    </div>
+  </>
+);
 
 export default ThirdPartyAuth;
