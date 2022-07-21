a3c4924c98cb67b8f5cb2dd488e94429611d049f
Ali <alivaraste@outlook.com>
2022-07-21 22:01:26 +0430

    GlobalFooter component is added

--- Components/HomePageWrapper/index.tsx
+++ Components/HomePageWrapper/index.tsx
@@ -1,38 +1,35 @@
-import {Wrapper} from './homePageWrapper.styles'
+import { Wrapper } from "./homePageWrapper.styles";
 import Sidebar from "../Sidebar";
 import SearchBar from "../SearchBar";
 import Trends from "../Trends";
 import WhoToFollow from "../WhoToFollow";
-import Footer from "../Home/Footer";
+import Footer from "../GlobalFooter";
 import PageWrapper from "../PageWrapper/PageWrapper";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "../../store/store";
 
-const HomePageWrapper = ({Component, title, props}) => {
+const HomePageWrapper = ({ Component, title, props }) => {
+  return (
+    <>
+      <Wrapper>
+        <Provider store={store}>
+          <div className="left">
+            <Sidebar />
+          </div>
+          <div className="main">
+            <PageWrapper title={title} props={props} Component={Component} />
+          </div>
 
-    return <>
-        <Wrapper>
-            <Provider store={store}>
-                <div className='left'>
-                    <Sidebar/>
-                </div>
-                <div className="main">
-                    <PageWrapper
-                        title={title}
-                        props={props}
-                        Component={Component}
-                    />
-                </div>
-
-                <div className='right'>
-                    <SearchBar/>
-                    <Trends/>
-                    <WhoToFollow/>
-                    <Footer/>
-                </div>
-            </Provider>
-        </Wrapper>
+          <div className="right">
+            <SearchBar />
+            <Trends />
+            <WhoToFollow />
+            <Footer />
+          </div>
+        </Provider>
+      </Wrapper>
     </>
-}
+  );
+};
 
 export default HomePageWrapper;
