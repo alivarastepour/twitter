8cb2676514ed10decbf389e374e55d0530fac182
Ali <alivaraste@outlook.com>
2022-07-23 18:43:22 +0430

    TfooterItem is moved to a separate file

---
+++ Components/Mainpage/Presenter/Footer/Handlers/TfooterItem.ts
@@ -0,0 +1,4 @@
+export type TfooterItems = {
+  id: number;
+  title: string;
+};
--- Components/Mainpage/Presenter/Footer/items.ts
+++ Components/Mainpage/Presenter/Footer/items.ts
@@ -1,25 +1,22 @@
-type Titem = {
-    id: number,
-    title: string
-}
+import { TfooterItems } from "./Handlers/TfooterItem";
 
-export const footerItems:Titem[] = [
-    {id: 0, title: "About"},
-    {id: 1, title: "Help Center"},
-    {id: 2, title: "Terms of Services"},
-    {id: 3, title: "Privacy Policy"},
-    {id: 4, title: "Cookie Policy"},
-    {id: 5, title: "Accessibility"},
-    {id: 6, title: "Ads info"},
-    {id: 7, title: "Blog"},
-    {id: 8, title: "Status"},
-    {id: 9, title: "Careers"},
-    {id: 10, title: "Brand Resources"},
-    {id: 11, title: "Advertising"},
-    {id: 12, title: "Marketing"},
-    {id: 13, title: "Twitter for business"},
-    {id: 14, title: "Developers"},
-    {id: 15, title: "Directory"},
-    {id: 16, title: "Settings"},
-    {id: 17, title: "© 2022 Twitter, Inc."},
-]
+export const footerItems: TfooterItems[] = [
+  { id: 0, title: "About" },
+  { id: 1, title: "Help Center" },
+  { id: 2, title: "Terms of Services" },
+  { id: 3, title: "Privacy Policy" },
+  { id: 4, title: "Cookie Policy" },
+  { id: 5, title: "Accessibility" },
+  { id: 6, title: "Ads info" },
+  { id: 7, title: "Blog" },
+  { id: 8, title: "Status" },
+  { id: 9, title: "Careers" },
+  { id: 10, title: "Brand Resources" },
+  { id: 11, title: "Advertising" },
+  { id: 12, title: "Marketing" },
+  { id: 13, title: "Twitter for business" },
+  { id: 14, title: "Developers" },
+  { id: 15, title: "Directory" },
+  { id: 16, title: "Settings" },
+  { id: 17, title: "© 2022 Twitter, Inc." },
+];
