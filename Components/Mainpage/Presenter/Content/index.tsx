30876c084c2b94909b7591354bc0f83d75e12821
Ali <alivaraste@outlook.com>
2022-07-23 18:44:10 +0430

    files are reformatted

--- Components/Mainpage/Presenter/Content/index.tsx
+++ Components/Mainpage/Presenter/Content/index.tsx
@@ -1,52 +1,52 @@
-import TwitterLogo from "../../../Logo/Twitter";
-
-import styles from "../../../../styles/Button/button.module.scss";
-
 import Link from "next/link";
+
+import TwitterLogo from "../../../Logo/Twitter";
 import ThirdPartyAuth from "../../../Buttons/ThirdPartyAuth";
-import {Button} from "../../../Buttons/MainpageButtonWrapper";
 import Divider from "../../../Divider";
 
+import { Button } from "../../../Buttons/MainpageButtonWrapper";
+import styles from "../../../../styles/Button/button.module.scss";
+
 const Content = () => (
-    <div className="mp-fl-2">
-        <div className="ct-ch ct-ch-1">
-            <TwitterLogo size="S"/>
-        </div>
-        <div className="ct-ch ct-ch-2 font font-sb">Happening now</div>
-        <div className="ct-ch ct-ch-3 font font-sb">Join Twitter today.</div>
-        <div className="ct-ch-4">
-            <Button className={`ct-ch-fl-1 ${styles.whiteButton}`}>
-                <ThirdPartyAuth company="Google" state="up"/>
-            </Button>
-            <Button className={`${styles.whiteButton} ct-ch-fl-2`}>
-                <ThirdPartyAuth company="Apple" state="up"/>
-            </Button>
-            <div className="ct-ch-fl-3">
-                <Divider orientation="horizontal" text="or" width={350} height={{}}/>
-            </div>
-            <Link href="/signup" passHref>
-                <Button className={`ct-ch-fl ${styles.blueButton}`}>
-                    <div className="text font font-eb">Sign up with phone or email</div>
-                </Button>
-            </Link>
-            <div className="ct-ch-fl-5">
-                <div className="font font-m notice">
-                    By signing up, you agree to the {"  "}{" "}
-                    <a href="#">Terms of Service</a>
-                    {"  "} and {"  "}
-                    <a href="#">Privacy Policy</a>, including <a href="#">Cookie Use</a>.
-                </div>
-            </div>
-        </div>
-        <div className="ct-ch-5">
-            <div className="font font-b text-q">Already have an account?</div>
-            <Link href="/signin" passHref>
-                <Button className={`ct-ch-fl ${styles.whiteBlueButton}`}>
-                    <div className={`font font-b`}>Sign in</div>
-                </Button>
-            </Link>
-        </div>
+  <div className="mp-fl-2">
+    <div className="ct-ch ct-ch-1">
+      <TwitterLogo size="S" />
     </div>
+    <div className="ct-ch ct-ch-2 font font-sb">Happening now</div>
+    <div className="ct-ch ct-ch-3 font font-sb">Join Twitter today.</div>
+    <div className="ct-ch-4">
+      <Button className={`ct-ch-fl-1 ${styles.whiteButton}`}>
+        <ThirdPartyAuth company="Google" state="up" />
+      </Button>
+      <Button className={`${styles.whiteButton} ct-ch-fl-2`}>
+        <ThirdPartyAuth company="Apple" state="up" />
+      </Button>
+      <div className="ct-ch-fl-3">
+        <Divider orientation="horizontal" text="or" width={350} height={-1} />
+      </div>
+      <Link href="/signup" passHref>
+        <Button className={`ct-ch-fl ${styles.blueButton}`}>
+          <div className="text font font-eb">Sign up with phone or email</div>
+        </Button>
+      </Link>
+      <div className="ct-ch-fl-5">
+        <div className="font font-m notice">
+          By signing up, you agree to the {"  "}{" "}
+          <a href="#">Terms of Service</a>
+          {"  "} and {"  "}
+          <a href="#">Privacy Policy</a>, including <a href="#">Cookie Use</a>.
+        </div>
+      </div>
+    </div>
+    <div className="ct-ch-5">
+      <div className="font font-b text-q">Already have an account?</div>
+      <Link href="/signin" passHref>
+        <Button className={`ct-ch-fl ${styles.whiteBlueButton}`}>
+          <div className={`font font-b`}>Sign in</div>
+        </Button>
+      </Link>
+    </div>
+  </div>
 );
 
 export default Content;
--- Components/Mainpage/Presenter/index.tsx
+++ Components/Mainpage/Presenter/index.tsx
@@ -1,46 +1,40 @@
-import React, {useContext, useEffect, useState} from "react";
-import {useRouter} from "next/router";
-import dynamic from 'next/dynamic';
+import React from "react";
+import dynamic from "next/dynamic";
 
 import {
-    Wrapper as MainWrapper,
-    Content as FooterWrapper,
+  Wrapper as MainWrapper,
+  Content as FooterWrapper,
 } from "../styles/mainpage.styles";
 import Spinner from "../../Spinner";
 
-import {authContext} from "../../../pages/_app";
+const HeroImage = dynamic(() => import("./HeroImage"), {
+  loading: () => <Spinner />,
+});
 
-const HeroImage = dynamic(
-    () => import("./HeroImage"),
-    {loading: () => <Spinner/>}
-)
+const Content = dynamic(() => import("./Content"), {
+  loading: () => <Spinner />,
+});
 
-const Content = dynamic(
-    () => import("./Content"),
-    {loading: () => <Spinner/>}
-)
+const Footer = dynamic(() => import("./Footer"), {
+  loading: () => <Spinner />,
+});
 
-const Footer = dynamic(
-    () => import('./Footer'),
-    {loading: () => <Spinner/>}
-)
+const Mainpage = ({ loading }) => (
+  <>
+    {!loading ? (
+      <>
+        <MainWrapper>
+          <HeroImage />
+          <Content />
+        </MainWrapper>
+        <FooterWrapper>
+          <Footer />
+        </FooterWrapper>
+      </>
+    ) : (
+      <Spinner />
+    )}
+  </>
+);
 
-const Mainpage = ({loading}) => {
-
-        return (
-            <>
-                {!loading ? <>
-                    <MainWrapper>
-                        <HeroImage/>
-                        <Content/>
-                    </MainWrapper>
-                    <FooterWrapper>
-                        <Footer/>
-                    </FooterWrapper>
-                </> : <Spinner/>
-                }
-            </>
-        );
-    }
-;
 export default Mainpage;
