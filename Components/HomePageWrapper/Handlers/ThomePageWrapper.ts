542d0afd7a0b7192cc2ea38fa5c327f40d404389
Ali <alivaraste@outlook.com>
2022-07-23 18:22:54 +0430

    prop type is added to HomePageWrapper component

---
+++ Components/HomePageWrapper/Handlers/ThomePageWrapper.ts
@@ -0,0 +1,5 @@
+export type ThomePageWrapper = {
+  title: string;
+  Component: JSX.Element;
+  props: Object;
+};
--- Components/HomePageWrapper/index.tsx
+++ Components/HomePageWrapper/index.tsx
@@ -1,35 +1,36 @@
-import { Wrapper } from "./homePageWrapper.styles";
+import { Provider } from "react-redux";
+
+import store from "../../store/store";
+
 import Sidebar from "../Sidebar";
 import SearchBar from "../SearchBar";
 import Trends from "../Trends";
 import WhoToFollow from "../WhoToFollow";
 import Footer from "../GlobalFooter";
 import PageWrapper from "../PageWrapper/PageWrapper";
-import { Provider } from "react-redux";
-import store from "../../store/store";
 
-const HomePageWrapper = ({ Component, title, props }) => {
-  return (
-    <>
-      <Wrapper>
-        <Provider store={store}>
-          <div className="left">
-            <Sidebar />
-          </div>
-          <div className="main">
-            <PageWrapper title={title} props={props} Component={Component} />
-          </div>
+import { Wrapper } from "./homePageWrapper.styles";
+import { ThomePageWrapper } from "./Handlers/ThomePageWrapper";
 
-          <div className="right">
-            <SearchBar />
-            <Trends />
-            <WhoToFollow />
-            <Footer />
-          </div>
-        </Provider>
-      </Wrapper>
-    </>
-  );
-};
+const HomePageWrapper = ({ Component, title, props }: ThomePageWrapper) => (
+  <>
+    <Wrapper>
+      <Provider store={store}>
+        <div className="left">
+          <Sidebar />
+        </div>
+        <div className="main">
+          <PageWrapper title={title} props={props} Component={Component} />
+        </div>
+        <div className="right">
+          <SearchBar />
+          <Trends />
+          <WhoToFollow />
+          <Footer />
+        </div>
+      </Provider>
+    </Wrapper>
+  </>
+);
 
 export default HomePageWrapper;
