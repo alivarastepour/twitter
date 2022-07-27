ee6aedbd9df0a56e795193a8cbd6240f09953693
Ali <alivaraste@outlook.com>
2022-07-27 21:12:22 +0430

    prop type is added to TrendItem component

---
+++ Components/TrendItem/Handlers/TtrendItem.ts
@@ -0,0 +1,5 @@
+export type TtrendItem = {
+  title: string;
+  content: string;
+  count: string;
+};
--- Components/TrendItem/index.tsx
+++ Components/TrendItem/index.tsx
@@ -1,11 +1,14 @@
-import {Wrapper} from "./trendItem.styles";
+import { Wrapper } from "./trendItem.styles";
+import { TtrendItem } from "./Handlers/TtrendItem";
 
-const TrendItem = ({title, content, count}) => (
-    <Wrapper>
-        <div className='ti-title font font-m ti-margin'>{title}</div>
-        <div className='ti-content font font-b ti-margin'>{content}</div>
-        <div className='ti-count font font-m ti-margin'>{count} {count && 'tweets'}</div>
-    </Wrapper>
-)
+const TrendItem = ({ title, content, count }: TtrendItem) => (
+  <Wrapper>
+    <div className="ti-title font font-m ti-margin">{title}</div>
+    <div className="ti-content font font-b ti-margin">{content}</div>
+    <div className="ti-count font font-m ti-margin">
+      {count} {count && "tweets"}
+    </div>
+  </Wrapper>
+);
 
 export default TrendItem;
