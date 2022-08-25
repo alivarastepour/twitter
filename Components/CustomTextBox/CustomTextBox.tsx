e8105415f660124a9415db6b10367a2e1c2697e7
Ali <alivaraste@outlook.com>
2022-08-25 17:07:16 +0430

    more logic for CustomTextBox component is added

--- Components/CustomTextBox/CustomTextBox.tsx
+++ Components/CustomTextBox/CustomTextBox.tsx
@@ -1,21 +1,36 @@
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import styles from "../../styles/CustomTextBox/customTextBox.module.scss";
 
-const CustomTextBox = ({ onChange, onClick }) => {
+const CustomTextBox = ({ onChange, onClick, setTweetText }) => {
   const [placeHolder, setPlaceHolder] = useState("What's happening?");
-
+  const secondRef = useRef(null);
   const ref = useCallback((node) => {
-    if (node) node.addEventListener("input", onChange);
+    if (node)
+      node.addEventListener("input", (e) => {
+        onChange(e, setTweetText)(secondRef.current);
+      });
   }, []);
 
   return (
-    <div
-      contentEditable
-      ref={ref}
-      id="x"
-      className={`font font-m ${styles.textBox}`}
-    >
-      {placeHolder}
+    <div className={styles.hello}>
+      <div
+        contentEditable
+        ref={ref}
+        id="x"
+        className={`font font-m ${styles.textBox}`}
+        // onClick={() => {
+        //   secondRef.current.style.display = "none";
+        // }}
+      >
+        {/* {"          a       "} */}
+      </div>
+      <div
+        className={styles.hi + " font font-m"}
+        contentEditable={false}
+        ref={secondRef}
+      >
+        {placeHolder}
+      </div>
     </div>
   );
 };
