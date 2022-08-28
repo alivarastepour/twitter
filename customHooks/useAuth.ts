30afdc978b5695607b7a4127f53d2a83d6aefd9a
Ali <alivaraste@outlook.com>
2022-08-28 12:24:16 +0430

    authentication logic in useAuth is improved

--- customHooks/useAuth.ts
+++ customHooks/useAuth.ts
@@ -1,24 +1,55 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import axios, { AxiosError } from "axios";
+
+import { HOST } from "../public/host";
 
 const useAuth = (url: string, forwarding: boolean): boolean => {
   const [loading, setLoading] = useState(true);
 
+  const [alert, fireAlert] = useState(false);
+
   const router = useRouter();
 
   useEffect(() => {
-    const navigate = async () => {
-      const routerCondition = forwarding
-        ? localStorage.getItem("__ut")
-        : !localStorage.getItem("__ut");
-      if (routerCondition) {
-        await router.push(url);
+    const storageHandler = (event: StorageEvent): void => {
+      if (event.key === "__ut")
+        if (event.oldValue !== event.newValue) fireAlert((prev) => !prev);
+    };
+    window.addEventListener("storage", storageHandler);
+    return () => window.removeEventListener("storage", storageHandler);
+  }, []);
+
+  useEffect(() => {
+    const tokenValidation = async () => {
+      if (!localStorage.getItem("__ut")) {
+        if (!forwarding) {
+          await router.push(url);
+        }
+      } else {
+        await axios
+          .get(`${HOST}/user`, {
+            headers: {
+              authorization: `Token ${localStorage.getItem("__ut")}`,
+            },
+          })
+          .then(async () => {
+            if (forwarding) {
+              await router.push(url);
+            }
+          })
+          .catch(async (error: AxiosError) => {
+            if (error.response.status === 403)
+              if (!forwarding) {
+                await router.push(url);
+              }
+          });
       }
       setLoading(false);
     };
 
-    navigate();
-  }, []);
+    tokenValidation();
+  }, [alert]);
 
   return loading;
 };
