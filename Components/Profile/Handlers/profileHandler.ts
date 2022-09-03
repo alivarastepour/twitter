716f24b4f6e726b2785dd42b95b66598b3f6cadd
Ali <alivaraste@outlook.com>
2022-09-03 19:21:47 +0430

    a minor bug is fixed

--- Components/Profile/Handlers/profileHandler.ts
+++ Components/Profile/Handlers/profileHandler.ts
@@ -1,4 +1,5 @@
 export const toUpper = (phrase: string): string => {
+  if (!phrase) return phrase;
   let ans: string = "";
   for (const word of phrase.split(" ")) {
     ans += word[0].toLocaleUpperCase() + word.slice(1) + " ";
